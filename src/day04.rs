1fd73afa1e4653abd0d0b91c9d375a098f5a0f5b
sigfalt <1689082+sigfalt@users.noreply.github.com>
2024-12-04 08:29:50 +0000

    feat: add day 4 part 2

--- src/day04.rs
+++ src/day04.rs
@@ -103,9 +103,67 @@ pub fn part1(input: &str) -> Result<u64> {
     Ok(result)
 }
 
+impl Direction {
+    pub const fn opposite(&self) -> Direction {
+        match self {
+            Direction::North => Direction::South,
+            Direction::Northeast => Direction::Southwest,
+            Direction::East => Direction::West,
+            Direction::Southeast => Direction::Northwest,
+            Direction::South => Direction::North,
+            Direction::Southwest => Direction::Northeast,
+            Direction::West => Direction::East,
+            Direction::Northwest => Direction::Southeast
+        }
+    }
+    pub const fn diagonals() -> [Direction; 4] {
+        [
+            Direction::Northeast,
+            Direction::Southeast,
+            Direction::Southwest,
+            Direction::Northwest
+        ]
+    }
+}
+
 pub fn part2(input: &str) -> Result<u64> {
-    let _ = input;
-    Ok(0)
+    let wordsearch: Grid<_> = parse(input)?.into();
+
+    const TARGET_START: &Letter = &Letter::A;
+    const DIR_SEARCH: [Direction; 4] = Direction::diagonals();
+    const TARGET_DIAGONAL: &Letter = &Letter::M;
+    const TARGET_OPPOSITE: &Letter = &Letter::S;
+
+    let result = wordsearch.indexed_iter().map(|((x, y), letter)| {
+        let (x, y) = (isize::try_from(x).unwrap(), isize::try_from(y).unwrap());
+        if letter == TARGET_START {
+            let found_patterns: u8 = DIR_SEARCH.into_iter().map(|dir| {
+                let (x_init_offset, y_init_offset) = dir.get_offset();
+                let (next_x, next_y) = (x + x_init_offset, y + y_init_offset);
+                if wordsearch.get(next_x, next_y).is_some_and(|diag_letter| diag_letter == TARGET_DIAGONAL) {
+                    let (x_opp_offset, y_opp_offset) = dir.opposite().get_offset();
+                    let (opp_x, opp_y) = (x + x_opp_offset, y + y_opp_offset);
+                    if wordsearch.get(opp_x, opp_y).is_some_and(|opp_letter| opp_letter == TARGET_OPPOSITE) {
+                        1
+                    } else {
+                        0
+                    }
+                } else {
+                    0
+                }
+            }).sum();
+
+            if found_patterns == 2 {
+                1
+            } else {
+                0
+            }
+        } else {
+            0
+        }
+    }).sum();
+
+    Ok(result)
 }
 
 #[cfg(test)]
@@ -131,7 +189,7 @@ MXMXAXMASX";
 
     #[test]
     fn test_part_two() -> Result<()> {
-        assert_eq!(0, part2(TEST)?);
+        assert_eq!(9, part2(TEST)?);
         Ok(())
     }
 }
