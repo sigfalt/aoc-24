9e051be097f540e9ca8f23b05c1e74243af927ac
sigfalt <1689082+sigfalt@users.noreply.github.com>
2024-12-13 06:02:31 +0000

    feat[12]: add day 12 part 2 solution

--- src/day12.rs
+++ src/day12.rs
@@ -78,9 +78,6 @@ struct RegionInfo {
 	perimeter: u64
 }
 impl RegionInfo {
-	// pub fn new(id: usize) -> Self {
-	// 	Self { id, area: 0, perimeter: 0 }
-	// }
 	pub fn with_size(id: usize, area: u64, perimeter: u64) -> Self {
 		Self { id, area, perimeter }
 	}
@@ -115,12 +112,12 @@ pub fn part1(input: &str) -> Result<u64> {
 			}
 
 			let region_area = region_cells.len();
-			let region_perimeter = region_cells.iter().map(|&(region_row, region_col)| {
+			let region_perimeter = region_cells.iter().map(|&(region_row, region_col)|
 				4 - Direction::values().into_iter()
 					.flat_map(|dir| dir.offset_from((region_row, region_col)))
 					.filter_map(|(nbr_row, nbr_col)| region_cells.get(&(nbr_row, nbr_col)))
 					.count() as u64
-			}).sum();
+			).sum();
 
 			regions.push(RegionInfo::with_size(region_id, region_area as u64, region_perimeter));
 		}
@@ -129,9 +126,116 @@ pub fn part1(input: &str) -> Result<u64> {
 	Ok(regions.into_iter().map(|region| region.area * region.perimeter).sum())
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+struct RegionInfoPartTwo {
+	id: usize,
+	area: u64,
+	sides: u64
+}
+impl RegionInfoPartTwo {
+	pub fn with_size(id: usize, area: u64, sides: u64) -> Self {
+		Self { id, area, sides }
+	}
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Edge {
+	Horizontal{ col: isize, inside_row: isize, outside_row: isize },
+	Vertical{ row: isize, inside_col: isize, outside_col: isize },
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum ContinuousEdge {
+	Horizontal{ start_col: isize, end_col: isize, inside_row: isize, outside_row: isize },
+	Vertical{ start_row: isize, end_row: isize, inside_col: isize, outside_col: isize }
+}
+
 pub fn part2(input: &str) -> Result<u64> {
-	let _ = input;
-	Ok(0)
+	let grid: Grid<_> = parse(input).into();
+
+	let mut regions = Vec::new();
+	grid.indexed_iter().for_each(|((grid_row, grid_col), grid_cell)| {
+		let (grid_row, grid_col) = (grid_row.try_into().unwrap(), grid_col.try_into().unwrap());
+		if grid_cell.region.get().is_none() {
+
+			let region_id = regions.len();
+			let region_plant = grid_cell.plant;
+			let mut region_cells = AHashSet::from([(grid_row, grid_col)]);
+			let mut region_cells_queue = BTreeSet::from([(grid_row, grid_col)]);
+
+			while let Some((row, col)) = region_cells_queue.pop_first() {
+				// does this cell exist? is it already assigned a region? is it the right plant?
+				if let Some(next_cell) = grid.get(row, col) {
+					if next_cell.region.get().is_none() && next_cell.plant == region_plant {
+						// add this cell to the current region
+						next_cell.region.set(Some(region_id));
+						region_cells.insert((row, col));
+
+						// add neighboring cells to queue
+						region_cells_queue.extend(Direction::values().into_iter()
+							.flat_map(|dir| dir.offset_from((row, col))));
+					}
+				}
+			}
+
+			let region_area = region_cells.len();
+			let edges = region_cells.iter().sorted_unstable().flat_map(|&(region_row, region_col)| {
+				// for this cell, find neighboring cells that are not part of this region
+				Direction::values().into_iter()
+					.flat_map(move |dir| dir.offset_from((region_row, region_col)))
+					.filter(|&(nbr_row, nbr_col)| region_cells.get(&(nbr_row, nbr_col)).is_none())
+					// and find the edges
+					.map(move |(nbr_row, nbr_col)| {
+						if nbr_row == region_row {
+							Edge::Vertical { row: region_row, inside_col: region_col, outside_col: nbr_col }
+						} else {
+							Edge::Horizontal { col: region_col, inside_row: region_row, outside_row: nbr_row }
+						}
+					})
+			}).collect_vec();
+			let distinct_edges = edges.into_iter().fold(Vec::new(), |mut edge_runs, next_edge| {
+				if let Some(matching_edge_ix) = edge_runs.iter().position(|&edge_run| {
+					// are these the same kind of edge? are they adjacent?
+					match (edge_run, next_edge) {
+						(ContinuousEdge::Horizontal { start_col, end_col, inside_row: run_inside_row, outside_row: run_outside_row },
+							Edge::Horizontal { col: edge_col, inside_row: edge_inside_row, outside_row: edge_outside_row }) => {
+							edge_inside_row == run_inside_row && edge_outside_row == run_outside_row && (edge_col == start_col - 1 || edge_col == end_col + 1)
+						},
+						(ContinuousEdge::Vertical { start_row, end_row, inside_col: run_inside_col, outside_col: run_outside_col },
+							Edge::Vertical { row: edge_row, inside_col: edge_inside_col, outside_col: edge_outside_col }) => {
+							edge_inside_col == run_inside_col && edge_outside_col == run_outside_col && (edge_row == start_row - 1 || edge_row == end_row + 1)
+						},
+						_ => false,
+					}
+				}) {
+					match (edge_runs.get_mut(matching_edge_ix).unwrap(), next_edge) {
+						(ContinuousEdge::Horizontal { start_col, end_col, .. }, Edge::Horizontal { col: edge_col, .. }) => {
+							*start_col = edge_col.min(*start_col);
+							*end_col = edge_col.max(*end_col);
+						},
+						(ContinuousEdge::Vertical { start_row, end_row, .. }, Edge::Vertical { row: edge_row, .. }) => {
+							*start_row = edge_row.min(*start_row);
+							*end_row = edge_row.max(*end_row);
+						},
+						_ => {panic!("found matching edge but second pattern match failed!");}
+					};
+				} else {
+					let new_edge_run = match next_edge {
+						Edge::Horizontal { col, inside_row, outside_row } =>
+							ContinuousEdge::Horizontal { start_col: col, end_col: col, inside_row, outside_row },
+						Edge::Vertical { row, inside_col, outside_col } =>
+							ContinuousEdge::Vertical { start_row: row, end_row: row, inside_col, outside_col }
+					};
+					edge_runs.push(new_edge_run);
+				}
+				edge_runs
+			}).len();
+
+			regions.push(RegionInfoPartTwo::with_size(region_id, region_area as u64, distinct_edges as u64));
+		}
+	});
+
+	Ok(regions.into_iter().map(|region| region.area * region.sides).sum())
 }
 
 #[cfg(test)]
