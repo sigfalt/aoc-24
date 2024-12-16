f5a46737743b459a5c906b04cfa0962e77017566
sigfalt <1689082+sigfalt@users.noreply.github.com>
2024-12-16 04:40:23 +0000

    feat[15]: add day 15 part 2 solution

--- src/day15.rs
+++ src/day15.rs
@@ -1,5 +1,7 @@
+use std::collections::{BTreeMap, BTreeSet};
 use anyhow::*;
 use grid::Grid;
+use itertools::Itertools;
 use nom::branch::alt;
 use nom::character::complete::{char, line_ending};
 use nom::combinator::{all_consuming, opt, value};
@@ -43,7 +45,7 @@ enum Direction {
 	West,
 }
 impl Direction {
-	pub fn get_offset(&self) -> (isize, isize) {
+	pub const fn get_offset(&self) -> (isize, isize) {
 		match self {
 			Direction::North => (-1, 0),
 			Direction::East => (0, 1),
@@ -145,9 +147,103 @@ pub fn part1(input: &str) -> Result<u64> {
 	Ok(gps_sum)
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+enum MapCellPartTwo {
+	Robot,
+	BoxLeft,
+	BoxRight,
+	Wall,
+	Empty
+}
+impl MapCellPartTwo {
+	pub const fn other_half(&self) -> Option<MapCellPartTwo> {
+		match self {
+			MapCellPartTwo::BoxLeft => Some(MapCellPartTwo::BoxRight),
+			MapCellPartTwo::BoxRight => Some(MapCellPartTwo::BoxLeft),
+			_ => None
+		}
+	}
+	pub const fn other_half_dir(&self) -> Option<Direction> {
+		match self {
+			MapCellPartTwo::BoxLeft => Some(Direction::East),
+			MapCellPartTwo::BoxRight => Some(Direction::West),
+			_ => None
+		}
+	}
+}
+
 pub fn part2(input: &str) -> Result<u64> {
-	let _ = input;
-	Ok(0)
+	let (map, directions) = parse(input);
+	let mut map: Grid<_> = map.into_iter().map(|row| row.into_iter().flat_map(|cell| {
+		match cell {
+			MapCell::Robot => vec![MapCellPartTwo::Robot, MapCellPartTwo::Empty],
+			MapCell::Box => vec![MapCellPartTwo::BoxLeft, MapCellPartTwo::BoxRight],
+			MapCell::Wall => vec![MapCellPartTwo::Wall, MapCellPartTwo::Wall],
+			MapCell::Empty => vec![MapCellPartTwo::Empty, MapCellPartTwo::Empty],
+		}
+	}).collect()).collect_vec().into();
+
+	let (robot_row, robot_col) = map.indexed_iter().find_map(|(grid_coords, &grid_cell)| {
+		if grid_cell == MapCellPartTwo::Robot {
+			Some(grid_coords)
+		} else {
+			None
+		}
+	}).unwrap();
+
+	let mut try_push = |(curr_row, curr_col), push_dir| -> (isize, isize) {
+		fn rec((curr_row, curr_col): (isize, isize), curr_cell: MapCellPartTwo, push_dir: Direction, grid: &Grid<MapCellPartTwo>,
+		       pushes: &mut BTreeMap<(isize, isize), MapCellPartTwo>, vacant: &mut BTreeSet<(isize, isize)>) -> bool {
+			let (dest_row, dest_col) = push_dir.offset_from((curr_row, curr_col)).unwrap();
+			let dest_cell_contents = *grid.get(dest_row, dest_col).unwrap();
+
+			let pushed = match (dest_cell_contents, push_dir) {
+				(MapCellPartTwo::BoxLeft | MapCellPartTwo::BoxRight, Direction::North | Direction::South) => {
+					// need to recurse twice, once for this half of the box and once for the other half
+					let other_box_half = dest_cell_contents.other_half().unwrap();
+					let other_box_half_coords = dest_cell_contents.other_half_dir().unwrap().offset_from((dest_row, dest_col)).unwrap();
+
+					rec((dest_row, dest_col), dest_cell_contents, push_dir, grid, pushes, vacant)
+						&& rec(other_box_half_coords, other_box_half, push_dir, grid, pushes, vacant)
+				},
+				(MapCellPartTwo::BoxLeft | MapCellPartTwo::BoxRight, _) => {
+					rec((dest_row, dest_col), dest_cell_contents, push_dir, grid, pushes, vacant)
+				},
+				(MapCellPartTwo::Empty, _) => true,
+				_ => false
+			};
+
+			if pushed {
+				vacant.insert((curr_row, curr_col));
+				pushes.insert((dest_row, dest_col), curr_cell);
+			}
+
+			pushed
+		}
+
+		let mut cells_to_empty = BTreeSet::new();
+		let mut pushes_to_process = BTreeMap::new();
+		let robot_moved = rec((curr_row, curr_col), MapCellPartTwo::Robot, push_dir, &map, &mut pushes_to_process, &mut cells_to_empty);
+		if robot_moved {
+			cells_to_empty.into_iter().for_each(|(row, col)| *map.get_mut(row, col).unwrap() = MapCellPartTwo::Empty);
+			pushes_to_process.into_iter().for_each(|((row, col), cell)| *map.get_mut(row, col).unwrap() = cell);
+			push_dir.offset_from((curr_row, curr_col)).unwrap()
+		} else {
+			(curr_row, curr_col)
+		}
+	};
+
+	let _ = directions.into_iter().fold((robot_row as isize, robot_col as isize), |robot_loc, direction| try_push(robot_loc, direction));
+
+	let gps_sum = map.indexed_iter().filter_map(|((cell_row, cell_col), &map_cell)| {
+		if map_cell == MapCellPartTwo::BoxLeft {
+			Some((cell_col + (100 * cell_row)) as u64)
+		} else {
+			None
+		}
+	}).sum();
+
+	Ok(gps_sum)
 }
 
 #[cfg(test)]
@@ -201,7 +297,7 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 
 	#[test]
 	fn test_part_two() -> Result<()> {
-		assert_eq!(0, part2(SMALL_TEST)?);
+		assert_eq!(9021, part2(LARGE_TEST)?);
 		Ok(())
 	}
 }
