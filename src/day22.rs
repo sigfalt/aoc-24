35462191a1e977c8d90de2daf5c0fa3578f8d0f7
sigfalt <1689082+sigfalt@users.noreply.github.com>
2024-12-23 22:10:48 +0000

    feat[22]: add day 22 part 2 solution

--- src/day22.rs
+++ src/day22.rs
@@ -1,5 +1,7 @@
 use std::iter::successors;
+use ahash::AHashMap;
 use anyhow::*;
+use itertools::Itertools;
 use nom::character::complete::{digit1, line_ending};
 use nom::combinator::{all_consuming, map_res};
 use nom::{Finish, IResult};
@@ -41,28 +43,57 @@ pub fn part1(input: &str) -> Result<u64> {
 }
 
 pub fn part2(input: &str) -> Result<u64> {
-	let _ = input;
-	Ok(0)
+	let seeds = parse(input);
+
+	let all_buyer_secrets = seeds.into_iter().map(|secret|
+		successors(Some(secret), |&secret| Some(generate_next_secret(secret))).take(2000)
+	);
+	let sequence_profit_map = all_buyer_secrets.fold(AHashMap::new(), |sequence_profit_map, secrets| {
+		let prices = secrets.map(|secret| (secret % 10) as i8);
+		let deltas = prices.clone().tuple_windows().map(|(first, second)| second - first);
+
+		// sequences are defined as an ordered collection of four price changes
+		let sequences = deltas.tuple_windows::<(_, _, _, _)>();
+
+		// we will never sell on the first four prices provided by a buyer as there will not yet be
+		// enough information to specify a sequence of four price changes on which to sell
+		let sequence_price_mappings = sequences.zip(prices.skip(4));
+
+		let sequence_price_map = sequence_price_mappings.fold(AHashMap::new(), |mut sequence_price_map, (sequence, price)| {
+			sequence_price_map.entry(sequence).or_insert(price);
+			sequence_price_map
+		});
+
+		sequence_price_map.into_iter().fold(sequence_profit_map, |mut profit_map, (sequence, price)| {
+			profit_map.entry(sequence)
+				.and_modify(|profit| { *profit += price as u64; })
+				.or_insert(price as u64);
+			profit_map
+		})
+	});
+
+	Ok(sequence_profit_map.into_values().max().unwrap())
 }
 
 #[cfg(test)]
 mod tests {
 	use crate::day22::*;
 
-	const TEST: &str = "1
-10
-100
-2024";
-
 	#[test]
 	fn test_part_one() -> Result<()> {
-		assert_eq!(37327623, part1(TEST)?);
+		assert_eq!(37327623, part1("1
+10
+100
+2024")?);
 		Ok(())
 	}
 
 	#[test]
 	fn test_part_two() -> Result<()> {
-		assert_eq!(0, part2(TEST)?);
+		assert_eq!(23, part2("1
+2
+3
+2024")?);
 		Ok(())
 	}
 }
