3f8239884182b71a5016155a5515bac8a11bb775
sigfalt <1689082+sigfalt@users.noreply.github.com>
2024-12-24 03:53:47 +0000

    feat[23]: add day 23 part 2 solution

--- src/day23.rs
+++ src/day23.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeSet;
 use ahash::{AHashMap, AHashSet};
 use anyhow::*;
+use itertools::Itertools;
 use nom::character::complete::{alpha1, char, line_ending};
 use nom::combinator::all_consuming;
 use nom::{Finish, IResult};
@@ -52,9 +53,71 @@ pub fn part1(input: &str) -> Result<usize> {
 	Ok(three_member_connections.len())
 }
 
-pub fn part2(input: &str) -> Result<u64> {
-	let _ = input;
-	Ok(0)
+pub fn part2(input: &str) -> Result<String> {
+	let connection_pairs = parse(input);
+
+	let connection_map = connection_pairs.into_iter().fold(AHashMap::new(), |mut connection_map, (node_one, node_two)| {
+		connection_map.entry(node_one)
+			.and_modify(|node_one_entry: &mut BTreeSet<_>| { node_one_entry.insert(node_two); })
+			.or_insert(BTreeSet::from([node_two]));
+		connection_map.entry(node_two)
+			.and_modify(|node_two_entry| { node_two_entry.insert(node_one); })
+			.or_insert(BTreeSet::from([node_one]));
+
+		connection_map
+	});
+
+	let max_clique = |set: &BTreeSet<_>| {
+		fn bron_kerbosch(r: BTreeSet<&str>, p: BTreeSet<&str>, x: BTreeSet<&str>, connection_map: &AHashMap<&str, BTreeSet<&str>>) -> BTreeSet<BTreeSet<String>> {
+			if p.is_empty() && x.is_empty() {
+				BTreeSet::from([BTreeSet::from_iter(r.into_iter().map(|str_slice| str_slice.to_string()))])
+			} else {
+				// let (pivot, pivot_neighbors) = p.union(&x)
+				// 	.map(|key| (key, connection_map.get(key).unwrap()))
+				// 	.min_by_key(|(_, nbr_set)| nbr_set.len()).unwrap();
+				// let rec_call = p.clone().into_iter().filter(|&key| !pivot_neighbors.contains(key))
+				// 	.try_fold((p, x), |(mut p, mut x), key| {
+				// 		let new_r = BTreeSet::from_iter(r.union(&BTreeSet::from([key])).cloned());
+				// 		let new_p = BTreeSet::from_iter(p.intersection(connection_map.get(key).unwrap()).cloned());
+				// 		let new_x = BTreeSet::from_iter(x.intersection(connection_map.get(key).unwrap()).cloned());
+				// 		let result = bron_kerbosch(new_r, new_p, new_x, connection_map);
+				// 		if let Some(result) = result {
+				// 			ControlFlow::Break(result)
+				// 		} else {
+				// 			p.remove(key);
+				// 			x.insert(key);
+				// 			ControlFlow::Continue((p, x))
+				// 		}
+				// });
+
+				let (_, _, result) = p.clone().into_iter()
+					.fold((p, x, BTreeSet::default()), |(mut p, mut x, mut results), key| {
+						let new_r = BTreeSet::from_iter(r.union(&BTreeSet::from([key])).cloned());
+						let new_p = BTreeSet::from_iter(p.intersection(connection_map.get(key).unwrap()).cloned());
+						let new_x = BTreeSet::from_iter(x.intersection(connection_map.get(key).unwrap()).cloned());
+						bron_kerbosch(new_r, new_p, new_x, connection_map).into_iter().for_each(|result| {
+							results.insert(result);
+						});
+
+						p.remove(key);
+						x.insert(key);
+
+						(p, x, results)
+					});
+				result
+			}
+		}
+
+		bron_kerbosch(BTreeSet::default(), set.clone(), BTreeSet::default(), &connection_map)
+	};
+
+	let all_nodes = BTreeSet::from_iter(connection_map.keys().cloned());
+	let max_clique_set = max_clique(&all_nodes);
+	// println!("max_clique_set: {max_clique_set:?}");
+	let maximum_clique = max_clique_set.into_iter().max_by_key(|clique| clique.len()).unwrap();
+	// println!("maximum_clique: {maximum_clique:?}");
+
+	Ok(maximum_clique.into_iter().join(","))
 }
 
 #[cfg(test)]
@@ -102,7 +165,7 @@ td-yn";
 
 	#[test]
 	fn test_part_two() -> Result<()> {
-		assert_eq!(0, part2(TEST)?);
+		assert_eq!("co,de,ka,ta", part2(TEST)?);
 		Ok(())
 	}
 }
